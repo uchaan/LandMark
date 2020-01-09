36e4e7281f3c3d24044d5538277784cea542ca92
youchanpark <pakyhc@gmail.com>
2020-01-09 21:36:27 +0900

    success

--- app/src/main/java/com/example/landmark/MainActivity.java
+++ app/src/main/java/com/example/landmark/MainActivity.java
@@ -1,21 +1,36 @@
 package com.example.landmark;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.cloud.FirebaseVisionCloudDetectorOptions;
+import com.google.firebase.ml.vision.cloud.landmark.FirebaseVisionCloudLandmark;
+import com.google.firebase.ml.vision.cloud.landmark.FirebaseVisionCloudLandmarkDetector;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionLatLng;
 
 import java.io.InputStream;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
     Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageView = (ImageView)findViewById(R.id.image);
+        textView = (TextView) findViewById(R.id.textView) ;
 
         button = (Button)findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -36,7 +52,10 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, 1);
             }
         });
+
+        // *********** ML Kit code *****************************
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -51,8 +70,64 @@ public class MainActivity extends AppCompatActivity {
                     Bitmap img = BitmapFactory.decodeStream(in);
                     in.close();
                     // 이미지 표시
+                    // *************************************************************
+                    FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(img);
                     imageView.setImageBitmap(img);
                     button.setText("another image");
+
+                    FirebaseVisionCloudDetectorOptions options =
+                            new FirebaseVisionCloudDetectorOptions.Builder()
+                                    .setModelType(FirebaseVisionCloudDetectorOptions.LATEST_MODEL)
+                                    .setMaxResults(15)
+                                    .build();
+
+
+                    FirebaseVisionCloudLandmarkDetector detector = FirebaseVision.getInstance()
+                            .getVisionCloudLandmarkDetector(options);
+                    // Or, to change the default settings:
+                    // FirebaseVisionCloudLandmarkDetector detector = FirebaseVision.getInstance()
+                    //         .getVisionCloudLandmarkDetector(options);
+
+                    Task<List<FirebaseVisionCloudLandmark>> result = detector.detectInImage(image)
+                            .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionCloudLandmark>>() {
+                                @Override
+                                public void onSuccess(List<FirebaseVisionCloudLandmark> firebaseVisionCloudLandmarks) {
+                                    // Task completed successfully
+                                    // ...
+
+                                    for (FirebaseVisionCloudLandmark landmark: firebaseVisionCloudLandmarks) {
+
+                                        Rect bounds = landmark.getBoundingBox();
+                                        String landmarkName = landmark.getLandmark();
+
+                                        textView.setText(landmarkName);
+
+                                        String entityId = landmark.getEntityId();
+                                        float confidence = landmark.getConfidence();
+
+                                        // Multiple locations are possible, e.g., the location of the depicted
+                                        // landmark and the location the picture was taken.
+                                        for (FirebaseVisionLatLng loc: landmark.getLocations()) {
+                                            double latitude = loc.getLatitude();
+                                            double longitude = loc.getLongitude();
+                                        }
+                                    }
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    // Task failed with an exception
+                                    // ...
+                                }
+                            });
+
+
+
+
+                    // *************************************************************
+
+
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
