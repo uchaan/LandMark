cab5b65fa5ce1f875429648d061bc5d221b32719
ssoymikey <ssoymikey@kaist.ac.kr>
2020-01-10 19:54:02 +0900

    recyclerview completed

--- app/src/main/java/com/example/landmark/MainActivity.java
+++ app/src/main/java/com/example/landmark/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.landmark;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -12,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -24,6 +27,7 @@ import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.common.FirebaseVisionLatLng;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
     ImageView imageView;
     Button button;
     TextView textView;
+    RecyclerView recyclerView;
+    List<FirebaseVisionCloudLandmark> init;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageView = (ImageView)findViewById(R.id.image);
-        textView = (TextView) findViewById(R.id.textView) ;
 
         button = (Button)findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -50,10 +55,19 @@ public class MainActivity extends AppCompatActivity {
 //                사진여러장 선택할수 있게 해줌.
 //                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
                 startActivityForResult(intent, 1);
+
             }
         });
 
-        // *********** ML Kit code *****************************
+        // 리사이클러뷰에 LinearLayoutManager 객체 지정.
+        recyclerView = findViewById(R.id.landmark_candidate_recyclerview) ;
+        recyclerView.setLayoutManager(new LinearLayoutManager
+                (getApplicationContext(), LinearLayoutManager.HORIZONTAL, false));
+
+        init = new ArrayList<>();
+        landmark_candidate_adapter adapter =
+                new landmark_candidate_adapter(init);
+        recyclerView.setAdapter(adapter) ;
     }
 
 
@@ -75,15 +89,15 @@ public class MainActivity extends AppCompatActivity {
                     imageView.setImageBitmap(img);
                     button.setText("another image");
 
-                    FirebaseVisionCloudDetectorOptions options =
-                            new FirebaseVisionCloudDetectorOptions.Builder()
-                                    .setModelType(FirebaseVisionCloudDetectorOptions.LATEST_MODEL)
-                                    .setMaxResults(15)
-                                    .build();
+//                    FirebaseVisionCloudDetectorOptions options =
+//                            new FirebaseVisionCloudDetectorOptions.Builder()
+//                                    .setModelType(FirebaseVisionCloudDetectorOptions.LATEST_MODEL)
+//                                    .setMaxResults(15)
+//                                    .build();
 
 
                     FirebaseVisionCloudLandmarkDetector detector = FirebaseVision.getInstance()
-                            .getVisionCloudLandmarkDetector(options);
+                            .getVisionCloudLandmarkDetector();
                     // Or, to change the default settings:
                     // FirebaseVisionCloudLandmarkDetector detector = FirebaseVision.getInstance()
                     //         .getVisionCloudLandmarkDetector(options);
@@ -95,21 +109,33 @@ public class MainActivity extends AppCompatActivity {
                                     // Task completed successfully
                                     // ...
 
-                                    for (FirebaseVisionCloudLandmark landmark: firebaseVisionCloudLandmarks) {
+                                    // 받아온 결과가 처음부터 NULL(랜드마크가 아님)
+                                    if(firebaseVisionCloudLandmarks.isEmpty()){
+                                        Toast.makeText(getApplicationContext(),
+                                                "다른 사진으로 재도전!", Toast.LENGTH_LONG).show();
+                                    }
+                                    else{
+                                        // 런던 시내 안인지 체크하고 아니면 알려주기
+                                        ArrayList<FirebaseVisionCloudLandmark> updated_result =
+                                                inLondonChecker(firebaseVisionCloudLandmarks);
+                                        if(updated_result.isEmpty()){
+                                            Toast.makeText(getApplicationContext(),
+                                                    "런던의 랜드마크가 아니네요! ^^", Toast.LENGTH_LONG).show();
 
-                                        Rect bounds = landmark.getBoundingBox();
-                                        String landmarkName = landmark.getLandmark();
+                                            // RecyclerView 초기화
+                                            landmark_candidate_adapter adapter =
+                                                    new landmark_candidate_adapter(init);
+                                            recyclerView.setAdapter(adapter) ;
+                                        }
 
-                                        textView.setText(landmarkName);
-
-                                        String entityId = landmark.getEntityId();
-                                        float confidence = landmark.getConfidence();
-
-                                        // Multiple locations are possible, e.g., the location of the depicted
-                                        // landmark and the location the picture was taken.
-                                        for (FirebaseVisionLatLng loc: landmark.getLocations()) {
-                                            double latitude = loc.getLatitude();
-                                            double longitude = loc.getLongitude();
+                                        // 여기 진입하면 런던시내안이고, 결과 NULL 아님
+                                        // top3 (또는 그 이하) RecyclerView 보여주기
+                                        else{
+                                            int min_item = Math.min(3, updated_result.size());
+//                                        init = updated_result.subList(0,min_item)
+                                            landmark_candidate_adapter adapter =
+                                                    new landmark_candidate_adapter(updated_result.subList(0,min_item));
+                                            recyclerView.setAdapter(adapter) ;
                                         }
                                     }
                                 }
@@ -119,6 +145,8 @@ public class MainActivity extends AppCompatActivity {
                                 public void onFailure(@NonNull Exception e) {
                                     // Task failed with an exception
                                     // ...
+                                    Toast.makeText(getApplicationContext(),
+                                            "랜드마크 인식에 실패했어요 ㅜㅜ", Toast.LENGTH_LONG).show();
                                 }
                             });
 
@@ -134,4 +162,29 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+    // 랜드마크의 위치가 런던 시내 이내인지 확인하고 sorted arraylist 반환
+    public ArrayList<FirebaseVisionCloudLandmark> inLondonChecker(List <FirebaseVisionCloudLandmark> arr){
+
+        ArrayList<FirebaseVisionCloudLandmark> updated_arr = new ArrayList<>();
+
+        // 받은 결과값이 null 이 아닌 경우
+        if (!arr.isEmpty()) {
+            //위도 경도 확인
+            for (FirebaseVisionCloudLandmark landmark : arr) {
+                for (FirebaseVisionLatLng loc : landmark.getLocations()) {
+                    double latitude = loc.getLatitude();
+                    double longitude = loc.getLongitude();
+
+                    if (longitude <= 0 && longitude >= -0.204021) {
+                        if (latitude <= 51.55 && latitude >= 51.48)
+                            updated_arr.add(landmark);
+                    }
+                }
+            }
+        }
+
+        return updated_arr;
+    }
+
 }
