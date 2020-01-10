72566e693354a0297fa81c4bbbc5785e6cfa5ab5
Youchan Park <46271173+uchaan@users.noreply.github.com>
2020-01-10 19:53:36 +0900

    크롭 +

--- app/src/main/java/com/example/landmark/MainActivity.java
+++ app/src/main/java/com/example/landmark/MainActivity.java
@@ -2,16 +2,22 @@ package com.example.landmark;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Rect;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.FileUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -22,8 +28,13 @@ import com.google.firebase.ml.vision.cloud.landmark.FirebaseVisionCloudLandmark;
 import com.google.firebase.ml.vision.cloud.landmark.FirebaseVisionCloudLandmarkDetector;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.common.FirebaseVisionLatLng;
+import com.yalantis.ucrop.UCrop;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,6 +42,11 @@ public class MainActivity extends AppCompatActivity {
     ImageView imageView;
     Button button;
     TextView textView;
+    private String currentPhotoPath = "";
+    FirebaseVisionImage image;
+
+
+    private final int GET_GALLERY_IMAGE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,16 +60,53 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-//                사진여러장 선택할수 있게 해줌.
-//                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-                startActivityForResult(intent, 1);
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent. setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, GET_GALLERY_IMAGE);
             }
         });
 
         // *********** ML Kit code *****************************
+    }
+
+    private void openCropActivity(Uri sourceUri, Uri destinationUri) {
+        UCrop.Options options = new UCrop.Options();
+        options.setCircleDimmedLayer(true);
+        options.setCropFrameColor(ContextCompat.getColor(this, R.color.colorAccent));
+        UCrop.of(sourceUri, destinationUri)
+                .withMaxResultSize(300, 300)
+                .withAspectRatio(5f, 5f)
+                .start(this);
+    }
+
+    private File getImageFile() throws IOException {
+        String imageFileName = "JPEG_" + System.currentTimeMillis() + "_";
+        File storageDir = new File(
+                Environment.getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_DCIM
+                ), "Camera"
+        );
+        System.out.println(storageDir.getAbsolutePath());
+        if (storageDir.exists())
+            System.out.println("File exists");
+        else
+            System.out.println("File not exists");
+        File file = File.createTempFile(
+                imageFileName, ".jpg", storageDir
+        );
+        currentPhotoPath = "file:" + file.getAbsolutePath();
+        return file;
+    }
+
+    private void imageFromPath(Context context, Uri uri) {
+        // [START image_from_path]
+
+        try {
+            image = FirebaseVisionImage.fromFilePath(context, uri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // [END image_from_path]
     }
 
 
@@ -61,6 +114,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode,resultCode,data);
         // Check which request we're responding to
+        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            try {
+
+                Uri selectedImageUri = data.getData();
+                File file = getImageFile();
+                Uri destinationUri = Uri.fromFile(file);
+
+//            imageView.setImageURI(selectedImageUri);
+                openCropActivity(selectedImageUri, destinationUri);
+
+            } catch (Exception e) {
+                Toast.makeText(this,"틀림!", Toast.LENGTH_LONG);
+            }
+
+        } else if (resultCode==RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
+            final Uri resultUri = UCrop.getOutput(data);
+            imageView.setImageURI(resultUri);
+
+            imageFromPath(this, resultUri);
+
+            FirebaseVisionCloudLandmarkDetector detector = FirebaseVision.getInstance()
+                    .getVisionCloudLandmarkDetector();
+
+            Task<List<FirebaseVisionCloudLandmark>> result = detector.detectInImage(image)
+                    .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionCloudLandmark>>() {
+                        @Override
+                        public void onSuccess(List<FirebaseVisionCloudLandmark> firebaseVisionCloudLandmarks) {
+                            // Task completed successfully
+                            // ...
+                            textView.setText(firebaseVisionCloudLandmarks.get(0).getLandmark());
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            // Task failed with an exception
+                            // ...
+                        }
+                    });
+
+
+
+
+
+        } else if (resultCode == UCrop.RESULT_ERROR) {
+            final Throwable cropError = UCrop.getError(data);
+        }
+        /*
         if (requestCode == 1) {
             // Make sure the request was successful
             if (resultCode == RESULT_OK) {
@@ -75,18 +176,8 @@ public class MainActivity extends AppCompatActivity {
                     imageView.setImageBitmap(img);
                     button.setText("another image");
 
-                    FirebaseVisionCloudDetectorOptions options =
-                            new FirebaseVisionCloudDetectorOptions.Builder()
-                                    .setModelType(FirebaseVisionCloudDetectorOptions.LATEST_MODEL)
-                                    .setMaxResults(15)
-                                    .build();
-
-
                     FirebaseVisionCloudLandmarkDetector detector = FirebaseVision.getInstance()
-                            .getVisionCloudLandmarkDetector(options);
-                    // Or, to change the default settings:
-                    // FirebaseVisionCloudLandmarkDetector detector = FirebaseVision.getInstance()
-                    //         .getVisionCloudLandmarkDetector(options);
+                            .getVisionCloudLandmarkDetector();
 
                     Task<List<FirebaseVisionCloudLandmark>> result = detector.detectInImage(image)
                             .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionCloudLandmark>>() {
@@ -94,24 +185,7 @@ public class MainActivity extends AppCompatActivity {
                                 public void onSuccess(List<FirebaseVisionCloudLandmark> firebaseVisionCloudLandmarks) {
                                     // Task completed successfully
                                     // ...
-
-                                    for (FirebaseVisionCloudLandmark landmark: firebaseVisionCloudLandmarks) {
-
-                                        Rect bounds = landmark.getBoundingBox();
-                                        String landmarkName = landmark.getLandmark();
-
-                                        textView.setText(landmarkName);
-
-                                        String entityId = landmark.getEntityId();
-                                        float confidence = landmark.getConfidence();
-
-                                        // Multiple locations are possible, e.g., the location of the depicted
-                                        // landmark and the location the picture was taken.
-                                        for (FirebaseVisionLatLng loc: landmark.getLocations()) {
-                                            double latitude = loc.getLatitude();
-                                            double longitude = loc.getLongitude();
-                                        }
-                                    }
+                                    textView.setText(firebaseVisionCloudLandmarks.get(0).getLandmark());
                                 }
                             })
                             .addOnFailureListener(new OnFailureListener() {
@@ -121,10 +195,6 @@ public class MainActivity extends AppCompatActivity {
                                     // ...
                                 }
                             });
-
-
-
-
                     // *************************************************************
 
 
@@ -133,5 +203,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+
+         */
     }
 }
